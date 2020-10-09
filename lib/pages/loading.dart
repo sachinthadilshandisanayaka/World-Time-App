22176ddea2b7b4849a083048049d7fd8958c40e0
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-10 01:35:59 +0530

    updated

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,3 +1,17 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Loading screen'),
+    );
+  }
+}
