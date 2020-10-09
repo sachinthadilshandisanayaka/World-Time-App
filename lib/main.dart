f57fafb1cd275b13fdb97c5f4a14c2b721f7211b
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-10 01:25:25 +0530

    create time app

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+// import 'dart:html';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+// import '/pages/loading.dart';
+import 'package:test02/pages/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
---
+++ lib/pages/loading.dart
@@ -0,0 +1,3 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
